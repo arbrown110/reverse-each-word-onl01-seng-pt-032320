16542d85f7ae870b3f4d860feb3c07c29b5f1e27
chic-caller-7520 <arbrown110@gmail.com>
2020-03-30 03:04:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,5 @@
 
 
-def reverse_each_word(String)
-  str= string.split (" ")
-  reverse =[]
-  string.size.times{reverse << str.each}
-  reverse.join
-end
-
 def reverse_each_word(phrase)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
